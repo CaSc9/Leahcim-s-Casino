b8ea941ca95b1d15e35ff6fa884de9373b121d7d
Carlo Berger <carlo_berger@outlook.de>
2024-11-22 13:08:23 +0100

    Initial Commit

---
+++ eval.py
@@ -0,0 +1,21 @@
+def parse_letters(hand):
+    parsing_m = {"J": 11, "Q": 12, "K": 13, "A": 14}
+    value = card[0]
+    print(value)
+    if value is str:
+        return parsing_m[value]
+    else:
+        return value
+
+
+# card = [value(2-10 + J=11 Q=12 K=13 A=14   ), suit(s,c,h,d)]
+def evaluation(personal_c, community_c):
+
+    hand = personal_c + community_c
+    # parse to int
+
+    hand_sorted_value = sorted(hand, key=lambda card: card[0])
+    print(hand, hand_sorted_value)
+
+
+evaluation([[2, "s"], ["Q", "s"]], [[5, "c"]])
