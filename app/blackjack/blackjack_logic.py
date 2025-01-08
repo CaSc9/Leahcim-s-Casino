1ce3a151855d9ad0ea7434c937c79d8d1e8aa544
tramtatam <yuri.meleshko@icloud.com>
2025-01-08 12:10:25 +0100

    added blackjack logic

--- app/blackjack/blackjack_logic.py
+++ app/blackjack/blackjack_logic.py
@@ -37,50 +37,31 @@ def total(hand):
                 total += 11
         else:
             total += card[0]
-
     return total
 
 
 def round():
-    global konto, double, bet
+    global konto, double, bet, split
     playerIn = True
     dealerIn = True
     my_hand = []
+    new_hand = []
     dealers_hand = []
+    split = "2"
     print(f"Dealer hat: {deal(dealers_hand, 1)},    X")
-    #print(f"Du hast: {deal(my_hand, 2)}\nTotal: {total(my_hand)}")
-    my_hand = [[5, "Hearts"], [5, "Spades"]]
-    total(my_hand)
-    print(my_hand)
+    print(f"Du hast: {deal(my_hand, 2)}\nTotal: {total(my_hand)}")
     hit_or_stand = "1"
     if my_hand[0][0] == my_hand[1][0]:
         split = input("Split hand? (Yes = 1, No = 2)\n")
         if split == "1":
-            new_hand = [my_hand.pop(0)]
-            while playerIn and dealerIn:
-                if total(new_hand) == 21:
-                    print("Blackjack! Du hast gewonnen!")
-                    konto += 2 * bet
-                    break
-                while hit_or_stand == "1":
-                    deal(new_hand, 1)
-                    print(f"Du hast: {new_hand}\nTotal: {total(new_hand)}")
-                    if total(new_hand) > 21:
-                        print("Bust! Du hast verloren!")
-                        break
-                    hit_or_stand = input("Hit: 1      Stand: 2\n")
-                if total(new_hand) > 21:
-                    break
-                elif hit_or_stand == "2":
-                    hit_or_stand = "1"
-                    break
-            print(new_hand)
+            new_hand.append(my_hand.pop())
+            print(f"Du hast: {my_hand}")
     if total(my_hand) != 21 and split != "1":
         double = input("Double: 1   No double: 2\n")
     else:
         double = "2"
     while playerIn and dealerIn:
-        if total(my_hand) == 21:
+        if total(my_hand) == 21 and split != "1":
             print("Blackjack! Du hast gewonnen!")
             konto += 2 * bet
             break
@@ -89,7 +70,7 @@ def round():
             bet *= 2
             deal(my_hand, 1)
             if total(my_hand) > 21:
-                print("Bust! Du hast verloren!")
+                print("You Bust!")
                 break
             hit_or_stand = "2"
         elif double != "1":
@@ -97,17 +78,35 @@ def round():
         while hit_or_stand == "1":
             deal(my_hand, 1)
             print(f"Du hast: {my_hand}\nTotal: {total(my_hand)}")
-            if total(my_hand) > 21:
-                print("Bust! Du hast verloren!")
+            if total(my_hand) >= 21:
                 break
             hit_or_stand = input("Hit: 1      Stand: 2\n")
         if total(my_hand) > 21:
-            break
-        elif hit_or_stand == "2":
+            print("You Bust!")
+        if split == "1" and total(new_hand) != 21:
+            print(f"2. Hand: {new_hand}")
+            while playerIn and dealerIn:
+                if total(new_hand) == 21:
+                    print("Blackjack! Du hast gewonnen!")
+                    konto += 2 * bet
+                    break
+                hit_or_stand = input("Hit: 1      Stand: 2\n")
+                while hit_or_stand == "1":
+                    deal(new_hand, 1)
+                    print(f"Du hast: {new_hand}\nTotal: {total(new_hand)}")
+                    if total(new_hand) >= 21:
+                        break
+                    hit_or_stand = input("Hit: 1      Stand: 2\n")
+                if total(new_hand) > 21:
+                    print("You Bust!")
+                    break
+                elif hit_or_stand == "2":
+                    break
+        if hit_or_stand == "2":
             while total(dealers_hand) <= 16:
                 deal(dealers_hand, 1)
             print(f"Dealer hat: {dealers_hand}    Total: {total(dealers_hand)}")
-            print(f"Du hast: {my_hand}       Total: {total(my_hand)}")
+            print(f"Du hast: {my_hand}    Total: {total(my_hand)}")
             playerIn = False
         if total(dealers_hand) > 21:
             print("Dealer busts! Du hast gewonnen!")
@@ -117,13 +116,26 @@ def round():
                 print("Unentschieden!")
                 konto += bet
             elif total(dealers_hand) < total(my_hand):
-                print("Du hast gewonnen!")
+                print("Du hast:  gewonnen!")
                 konto += 2 * bet
             else:
                 print("Du hast verloren!")
             break
+    if split == "1":
+        print(f"2. Hand: {new_hand}    Total: {total(new_hand)}")
+        if total(dealers_hand) > 21:
+            print("Gewinnt ebenfalls!")
+            konto += 2 * bet
+        else:
+            if total(dealers_hand) == total(new_hand):
+                print("Unentschieden!")
+            elif total(dealers_hand) < total(new_hand):
+                print("Gewinnt!")
+                konto += 2 * bet
+            else:
+                print("Verliert!")
+                konto -= bet
         return konto
-
 
 konto = 1000
 
